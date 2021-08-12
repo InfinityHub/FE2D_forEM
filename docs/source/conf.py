aa60e74dcb51633dea95266a81a0ae3032a51783
Jianbo <InfinityHub@users.noreply.github.com>
2021-08-12 20:15:06 -0230

    Update conf.py
    try html_theme in conf.py 'sphinx_rtd_theme' (read the docs theme)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,9 +45,9 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
+# a list of builtin themes. E.g., 'alabaster','claasic','sphinx_rtd_theme'
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
